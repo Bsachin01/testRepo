0fcb9c0361728c2428f1afc986d8858947c369e8
JSW Admin <jswadmin@JSWs-MacBook-Pro-6.local>
2022-11-09 14:29:28 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mein hu Sachin Bharti
-        </p>
+          Mein hu Sachin 
+rocking        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
